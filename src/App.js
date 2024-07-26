fc9550244023faf1228efc8b75772782caa744ec
maydindev <maydin.dev@outlook.com>
2024-07-26 15:15:57 +0300

    Added values with props.

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 // Statik değerleri alın ve prop'ları kullanarak dinamik hale getirin
 // Örneğin: label id, label text, input placeholder, input description
-function FormInput() {
+function FormInput({ htmlFor, text, placeholder, description }) {
   return (
-    <div className='max-w-sm p-8'>
+    <div className="max-w-sm p-8">
       <label
-        htmlFor='email'
-        className='block text-sm font-medium leading-6 text-gray-900'
+        htmlFor={htmlFor}
+        className="block text-sm font-medium leading-6 text-gray-900"
       >
-        Email
+        {text}
       </label>
-      <div className='mt-2'>
+      <div className="mt-2">
         <input
-          id='email'
-          name='email'
-          type='email'
-          placeholder='you@example.com'
-          className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2'
+          id="email"
+          name="email"
+          type="email"
+          placeholder={placeholder}
+          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
         />
       </div>
-      <p className='mt-2 text-sm text-gray-500'>
-        Bunu sadece spam için kullanacağız.
+      <p className="mt-2 text-sm text-gray-500">
+        {description}
       </p>
     </div>
-  )
+  );
 }
 
 export default function App() {
   return (
     <form>
-      <FormInput />
-      <FormInput />
+      <FormInput htmlFor="email" text="Email" placeholder="you@example.com" description="Bunu sadece spam için kullanacağız."/>
+      <FormInput htmlFor="email" text="Email" placeholder="i@example.com" description="Bunu sadece spam için kullanacağız."/>
+      <FormInput htmlFor="email" text="Email" placeholder="we@example.com" description="Bunu biz kullanacağız."/>
     </form>
-  )
+  );
 }
